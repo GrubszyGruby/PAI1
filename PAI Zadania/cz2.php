e024fdb503ebd919250df2d5f97134cfdb02da0e
GrubszyGruby <wwesk@o2.pl>
2022-09-14 11:44:58 +0200

    Komentarz

---
+++ PAI Zadania/cz2.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+$x = 10;
+$y = 0;
+if ( $y + $x == 10) 
+	echo $x;
+if ( $y + $x == 0)
+	echo $y;
+?>
+</body>
+</html>
---
+++ PAI Zadania/konkatenacja.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+$zmienna = 5;
+echo "masz". $zmienna. "wiadomosci"
+if ($zmienna == 0){
+
+	echo "brak wiadmosci"
+}
+else echo "masz 5 wiadmosci";
+?>
+</body>
+</html>
