94cb4e3a21be5ee75532506ace1d0798af9402b2
Wiktor Kusztykiewicz (Uczeń) <wkusztykiewicz175@technikumkreatywne.pl>
2022-09-26 13:02:18 +0200

    Komentarz

---
+++ PAI Zadania/tablica asociacyjna.php
@@ -0,0 +1,13 @@
+<?php
+$pp = array ("xd",  "xdd", "xddd", "xdddd");
+$j = 0;
+
+foreach($pp as $ebe){
+echo "$j: $ebe <br>";
+++$j; }	
+
+echo "xd<br>";
+echo "xdddd";
+	
+  
+?>
---
+++ PAI Zadania/tablice.php
@@ -0,0 +1,9 @@
+<?php
+    $pp[] = "Hobbit";
+    $pp[] = "LOTR";
+    $pp[] = "Maly ksiaze";
+
+    for($j =2; $j >=0; --$j){
+        echo "$j: $pp[$j] <br>";
+    }
+?>
